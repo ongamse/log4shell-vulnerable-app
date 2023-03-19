db95611baa12fc4df7722f9bd69d150f26b5d7e3
Magno Gomes <117935539+ongamse@users.noreply.github.com>
2023-03-19 01:31:37 -0400

    Update VulnServlet.java

--- src/main/java/dvl4wa/VulnServlet.java
+++ src/main/java/dvl4wa/VulnServlet.java
@@ -7,14 +7,24 @@ import java.io.Writer;
 import java.util.Map;
 import java.util.Collections;
 import java.util.stream.Collectors;
+//import org.apache.logging.log4j.Logger;
+//import org.apache.logging.log4j.LogManager;
 
 public class VulnServlet extends HttpServlet {
   protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException {
+    //Logger logger = LogManager.getLogger(VulnServlet.class);
     try {
       Map<String, String> headers = Collections.list(req.getHeaderNames()).stream().collect(Collectors.toMap(h -> h, req::getHeader));
       res.setContentType("text/plain; charset=utf-8");
       Writer writer = res.getWriter();
       if(headers.containsKey("x-log")) {
+        writer.write("Logging to console using vulnerable log4j2!\n");
+
+        // Log user supplied value without validation
+        // logger.info(headers.get("x-log"));
+
+        // Log static text
+        // logger.info("Header contains value for x-log");
         writer.write("Logging to console using vulnerable log4j2!\n");     
       } else {
         writer.write("Hello world\n");
